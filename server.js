db40f952dedce1df27ebd8584fa1205f26c5729d
Rich Clarke <rich@scrich.co.uk>
2017-03-22 22:32:00 +0000

    breakout api controllers

--- server.js
+++ server.js
@@ -1,41 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Post= require('./models/post')
 
 var app = express()
 app.use(bodyParser.json())
+app.use('/api/posts', require('./controllers/api/posts'))
 
 // GET the flat html page
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/layouts/posts.html')
-})
-
-// GET from the database
-
-app.get('/api/posts', function (req,res,next) {
-    Post.find()
-    .sort('-date')
-    .exec(function(err, posts) {
-        if (err) {return next (err)}
-        res.json(posts)
-    })
-})
-
-// Post a new comment
-
-var Post= require('./models/post')
-
-app.post('/api/posts', function (req,res, next) {
-    var post = new Post ({
-        username: req.body.username,
-        body: req.body.body
-    })
-    post.save(function(err,post) {
-        if (err) {
-            return next(err)
-        }
-        res.status(201).json(post)
-    })
 })
 
 app.listen(3000,function() {
