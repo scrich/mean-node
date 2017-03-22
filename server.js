c9d9a6aefdaa3e36d6559110ae41eb9611b23f2d
Rich Clarke <rich@scrich.co.uk>
2017-03-22 22:49:27 +0000

    breakout homepage to static.js

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ var Post= require('./models/post')
 
 var app = express()
 app.use(bodyParser.json())
+
 app.use('/api/posts', require('./controllers/api/posts'))
-
-// GET the flat html page
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/layouts/posts.html')
-})
+app.use( require('./controllers/static'))
+// equivalent to app.use('/', require ('./controllers/static'))
 
 app.listen(3000,function() {
     console.log('Server listening on ', 3000)
