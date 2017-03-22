bc0f1ae01894b184224497644674de798156a017
Rich Clarke <rich@scrich.co.uk>
2017-03-22 22:10:44 +0000

    post ordering in server and client

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.get('/', function (req, res) {
 // GET from the database
 
 app.get('/api/posts', function (req,res,next) {
-    Post.find(function(err, posts) {
+    Post.find()
+    .sort('-date')
+    .exec(function(err, posts) {
         if (err) {return next (err)}
         res.json(posts)
     })
